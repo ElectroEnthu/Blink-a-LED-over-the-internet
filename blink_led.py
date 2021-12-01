1ce6835dfae4da425633d94a6be95f07692a2312
Harsh Gupta <95281302+ElectroEnthu@users.noreply.github.com>
2021-12-01 16:47:13 +0530

    Create blink_led.py

---
+++ blink_led.py
@@ -0,0 +1,12 @@
+from boltiot import Bolt
+import conf,time
+
+mybolt = Bolt(conf.api_key,conf.device_id)
+
+while True:
+    on = mybolt.digitalWrite('0','HIGH')
+    print(on)
+    time.sleep(2) #1 second delay
+    off = mybolt.digitalWrite('0','LOW')
+    print(off)
+    time.sleep(2) #1 second delay
