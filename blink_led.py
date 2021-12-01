34fe1eebaf2d3efdb79f2c9a5dc30d65c11bed1f
Harsh Gupta <95281302+ElectroEnthu@users.noreply.github.com>
2021-12-01 16:49:33 +0530

    Update blink_led.py

--- blink_led.py
+++ blink_led.py
@@ -6,7 +6,7 @@ mybolt = Bolt(conf.api_key,conf.device_id)
 while True:
     on = mybolt.digitalWrite('0','HIGH')
     print(on)
-    time.sleep(2) #1 second delay
+    time.sleep(1) #1 second delay
     off = mybolt.digitalWrite('0','LOW')
     print(off)
-    time.sleep(2) #1 second delay
+    time.sleep(1) #1 second delay
